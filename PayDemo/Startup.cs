fc097b1a654ffb1a72d7d314e61e2ba490bf877e
tan3000 <tan3000@163.com>
2020-09-17 10:01:24 +0800

    加入get、post方式的远程调用

--- PayDemo/Startup.cs
+++ PayDemo/Startup.cs
@@ -48,7 +48,7 @@ namespace PayDemo
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
-            //全局跨域访问
+            //全局跨域访问（重要）
             app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
             app.UseMvc(routes =>
             {
